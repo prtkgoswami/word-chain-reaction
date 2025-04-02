084e02e8375e342eb40c34ad6a08fbd3f6878887
Pratik Goswami <prtkgoswami8@gmail.com>
2025-04-02 01:10:58 -0700

    Configure environment variables for Firebase and update gitignore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbx3NwLVZa6RDWRmc-B9g0g162hty40hU",
-  authDomain: "word-chain-reaction.firebaseapp.com",
-  projectId: "word-chain-reaction",
-  storageBucket: "word-chain-reaction.firebasestorage.app",
-  messagingSenderId: "793707252472",
-  appId: "1:793707252472:web:a85faa517f53ef2c2ac90d",
-  measurementId: "G-BL09F6ZYRL",
-  databaseURL: "https://word-chain-reaction-default-rtdb.firebaseio.com/"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
